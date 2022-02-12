1225ab63013dd134634cee73f4f8dcfdd97ed889
tq0066 <60831189+tliu0066@users.noreply.github.com>
2022-02-12 10:56:12 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let weather = {
+    "apikey" : "355b5a983bebc0652fbc6ad1adaf8842",
+    fetchWeather:function(city){
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city
+            + "&units=metric&appid=" + this.apikey
+        )
+        .then((response) => response.json())
+        .then((data)=>this.displayWeather(data));
+    },
+    displayWeather: function(data){
+        const { name } = data;
+        const { icon, description} = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description,temp, humidity, speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp;
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + "Km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/2560x1400/?" + name + "')"
+       
+    },
+
+    search: function(){
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+}
+
+document
+    .querySelector(".search button")
+    .addEventListener("click", function(){
+        weather.search()
+
+});
+
+document
+.querySelector(".search-bar")
+.addEventListener("keyup", function(event){
+    if(event.key = "Enter"){
+        weather.search();
+    }
+});
+
+weather.fetchWeather()
